6aa70e6ab560e094e40d308fd23c1a9522be855e
skullsThd <104901809+skullsThd@users.noreply.github.com>
2023-06-10 15:04:54 +0200

    Create access_control.c

---
+++ access_control.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool authorize(const char *user, const char *access_level) {
+    // Codice per l'autorizzazione dell'utente
+    return true;
+}
+
+bool authenticate(const char *username, const char *password) {
+    // Codice per l'autenticazione dell'utente
+    return true;
+}
